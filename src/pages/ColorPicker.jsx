a871c65edb26564c588c73e6b6b51b5241907302
Nardika Kurniawan <nardika.kurniawan@lumoshive.com>
2022-11-17 14:50:39 +0700

    color picker

---
+++ src/pages/ColorPicker.jsx
@@ -0,0 +1,52 @@
+import React from "react";
+import { ColorPickerComponent } from "@syncfusion/ej2-react-inputs";
+
+import { kanbanData, kanbanGrid } from "../data/dummy";
+import { Header } from "../components";
+
+const ColorPicker = () => {
+  const change = (args) => {
+    document.getElementById("preview").style.backgroundColor =
+      args.currentValue.rgba;
+
+    // console.log(args.currentValue);
+  };
+
+  return (
+    <div className="m-2 md:m-10 mt-40 p-2 md:p-10 bg-white rounded-3xl">
+      <Header category="App" title="Color Picker" />
+
+      <div className="text-center">
+        <div className="" id="preview" />
+
+        <div className="flex justify-center items-center gap-20 flex-wrap">
+          <div className="">
+            <p className="text-2xl font-semibold mt-2 mb-4">Inline Pallete</p>
+            <ColorPickerComponent
+              id="inline-pallete"
+              mode="Palette"
+              modeSwitcher={false}
+              inline
+              showButtons={false}
+              change={change}
+            />
+          </div>
+
+          <div className="">
+            <p className="text-2xl font-semibold mt-2 mb-4">Inline Picker</p>
+            <ColorPickerComponent
+              id="inline-pallete"
+              mode="Picker"
+              modeSwitcher={false}
+              inline
+              showButtons={false}
+              change={change}
+            />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default ColorPicker;
