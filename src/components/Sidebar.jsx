3f1fb76a03ea4420fbedbbc2c287783920062d29
Nardika Kurniawan <nardika.kurniawan@lumoshive.com>
2022-11-18 09:58:39 +0700

    Theme Fixes

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -8,13 +8,16 @@ import { links } from "../data/dummy";
 import { useStateContext } from "../contexts/ContextProvider";
 
 const Sidebar = () => {
-  const { activeMenu, setActiveMenu, screenSize } = useStateContext();
+  const { activeMenu, setActiveMenu, screenSize, currentColor } =
+    useStateContext();
 
   const handleCloseSideBar = () => {
     if (activeMenu && screenSize <= 900) {
       setActiveMenu(false);
     }
   };
+
+  console.log(currentColor);
 
   const activeLink =
     "flex item-center gap-5 pl-4 pt-3 pb-2.5 rounded-lg text-white text-md m-2";
@@ -54,6 +57,9 @@ const Sidebar = () => {
                     to={`/${link.name}`}
                     key={link.name}
                     onClick={handleCloseSideBar}
+                    style={({ isActive }) => ({
+                      backgroundColor: isActive ? currentColor : "",
+                    })}
                     className={({ isActive }) =>
                       isActive ? activeLink : normalLink
                     }
